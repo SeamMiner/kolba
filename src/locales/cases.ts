3764209f28d1932f288f07daa498f41d98142424
mazgavan <62303186+mazgavan@users.noreply.github.com>
2023-05-02 20:14:57 +0300

    Update cases.ts

--- src/locales/cases.ts
+++ src/locales/cases.ts
@@ -147,7 +147,7 @@ const projects = {
   uretra: {
     title_key: "uretra",
     image: "uretra",
-    tags: ["other"],
+    tags: ["other", "best"],
     subtags: ['logos'],
     info: {
       type: "Logo",
